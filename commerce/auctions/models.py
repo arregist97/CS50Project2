1990d87eb7351d833b5eb45f4e7b67a5eba3c244
RicoRicoCA <rregister@gmail.com>
2020-09-22 19:03:51 -0700

    Added fields to User model, and registered User in admin.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,11 +2,18 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    pass
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=65536)
     current_price = models.FloatField()
     photo = models.BinaryField()
+
+    def __str__(self):
+        return f"{self.title}(${self.current_price})"
+
+class User(AbstractUser):
+    auctions_completed = models.IntegerField()
+    listings = models.ManyToManyField(Listing, blank=True,related_name="listings")
     
+    def __str__(self):
+        return f"{self.username}({self.auctions_completed})"
