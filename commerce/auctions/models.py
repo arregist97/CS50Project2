324ac3b3d3820c617d9bf5453e7bff0766c19ba8
RicoRicoCA <rregister@gmail.com>
2020-09-22 19:57:36 -0700

    Commented out the seller field from Listing in models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,18 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class User(AbstractUser):
+    auctions_completed = models.PositiveIntegerField(default=0) #Starts at zero, increments w each successful sell/buy
+    
+    def __str__(self):
+        return f"{self.username}({self.auctions_completed})"
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=65536)
     current_price = models.FloatField()
+#   seller = models.ForeignKey(User, on_delete=models.CASCADE)
     photo = models.BinaryField()
 
     def __str__(self):
-        return f"{self.title}(${self.current_price})"
+        return f"{self.title}:{self.seller} (${self.current_price})"
 
-class User(AbstractUser):
-    auctions_completed = models.IntegerField()
-    listings = models.ManyToManyField(Listing, blank=True,related_name="listings")
-    
-    def __str__(self):
-        return f"{self.username}({self.auctions_completed})"
