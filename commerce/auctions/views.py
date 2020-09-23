11207df06451c3d2ae027c2437ef16b242d792e7
RicoRicoCA <rregister@gmail.com>
2020-09-23 14:25:53 -0700

    Added links for items on index page

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -78,7 +78,7 @@ def listing(request):
 def listing_view(request, listing_id):
     items = Listing.objects.all()
     try:
-        item = items.get(id=listing_id)
+        item = items.get(pk=listing_id)
     except:
         return HttpResponse("Error: item number (" + listing_id + ") not found.<br>" + "<a href=" + "/" + ">Home</a>")
     return render(request, "auctions/listing_view.html", {
